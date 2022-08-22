855af60c7a866a3a002da9ffda4cd5ab88872dd2
Robin Tapiru <robintapiru0894@gmail.com>
2022-08-22 20:36:17 +0400

    gallery design 80%

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,29 +53,6 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 };
 
-// show list of monkey
-var allPics = 39;
-
-for(i=0;i<=allPics; i++){
-  // myImg.src = images[i];
-
-  // TODO: adjust this to whatever you want
-  // in this example, use `<a>` that link to another page
-  // you can use javascript to show modal/alert too
-  var picture = document.createElement('picture');
-
-  var img = document.createElement('img');
-  img.src = './assets/images/lac-nft/'+[i]+'.png';
-  picture.appendChild(img);
-
-  if(i > 19) {
-    document.getElementById('slide-lac-cont-2').appendChild(picture); 
-  } else {
-    document.getElementById('slide-lac-cont-1').appendChild(picture);
-  }
-
-}
-
 
 // slide menu 
 
